6a01828d4c9ca4210328f96cf35c14294e64a87c
alexsaxberg <70248055+alexsaxberg@users.noreply.github.com>
2020-12-20 14:46:00 -0800

    Add files via upload

---
+++ molemass.py
@@ -0,0 +1,5 @@
+mol=["H","C","N","O","P","S","K","F"]
+molemasses=[1.088,12.0107,14.00674,15.9994,30.973761,32.066,39.0983,18.9984]
+mols=dict(zip(mol,molemasses))
+print(float(molemasses[1])+(float(molemasses[2]))
+
